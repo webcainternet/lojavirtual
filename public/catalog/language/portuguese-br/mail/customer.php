253c799552061535638a05480dd542024e3a4549
fernandofmendes <fernando.mendes@webca.com.br>
2015-01-20 10:45:28 -0200

    mail welcome

--- public/catalog/language/portuguese-br/mail/customer.php
+++ public/catalog/language/portuguese-br/mail/customer.php
@@ -1,7 +1,7 @@
 <?php
 // Text
 $_['text_subject']        = '%s - Obrigado por se registrar!';
-$_['text_welcome']        = '<a href="https://lojavirtual.digital" title="LojaVirtual .digital"><img src="https://lojavirtual.digital/image/catalog/lojalogo_1_mail.png" alt="LojaVirtual .digital" style="margin-bottom: 20px; border: none;" /></a><br>Obrigado por se registrar na loja %s!';
+$_['text_welcome']        = 'Obrigado por se registrar na loja %s!';
 $_['text_login']          = 'Sua conta foi criada, e você poderá acessar sua conta utilizando seu e-mail e senha ao visitar nossa loja, ou através do seguinte link:';
 $_['text_approval']       = 'Sua conta precisa ser aprovada para que você possa acessar seus dados. Assim que for aprovada você poderá acessar utilizando seu endereço de e-mail e sua senha ao vistar nossa loja, ou através do seguinte link:';
 $_['text_services']       = 'Ao acessar sua conta você poderá acessar outros serviços, inclusive visualizar compras anteriores, imprimir faturas e editar as informações de sua conta.';
