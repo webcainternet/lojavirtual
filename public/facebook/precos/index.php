91d7a8b085fac0893c6cc5285ab1ea0bdf540a8f
root <root@uspro575.startdedicated.com>
2015-01-21 16:43:52 -0200

    aba face

---
+++ public/facebook/precos/index.php
@@ -0,0 +1,153 @@
+<html>
+
+<head>
+<meta charset="UTF-8" />
+
+<link href='//fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+<link href="//fonts.googleapis.com/css?family=Open+Sans:400,400i,300,700" rel="stylesheet" type="text/css" />
+<link href="/catalog/view/javascript/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen" />
+<link href="/catalog/view/theme/default/stylesheet/stylesheet.css" rel="stylesheet">
+
+
+
+<style type="text/css">
+	body {
+		
+	}
+
+	.wca-iconshome {
+		text-align: center;
+		display: inline-block;
+		width: 190px;
+		height: 85px;
+		margin: 5px;
+		margin-top: 5px;
+		border-radius: 5px;
+		padding-top: 15px;
+		font-weight: 300;
+		font-size: 16px;
+	}
+	.wca-iconshome:hover {
+		background-color: white;
+	}
+	.container {
+		padding-left: 0px;
+		padding-right: 0px;
+		width: 100%;
+		background-color: rgba(242,242,242,0.9);
+	}
+	.footer-contato {
+		font-size: 18px;
+	}
+	.wca-footer-mid {
+		width: 390px;
+	}	
+	footer {
+		position: absolute;
+		bottom: 0px;
+	}
+</style>
+
+<!--Start of Zopim Live Chat Script-->
+<script type="text/javascript">
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+$.src='//v2.zopim.com/?2cQwfyblujZ70I5VuozVJQAV8RxfHpLn';z.t=+new Date;$.
+type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+</script>
+<!--End of Zopim Live Chat Script-->
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-57351164-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</head>
+
+<body>
+
+  <div class="container">   
+    <div style="float: left; width: 100%;">
+      <div class="wca-iconshome">
+        <a href="http://lojavirtual.digital/profissional" target="_blank">
+          <div><img src="/image/catalog/webca/home-nav-tools.png"></div>
+          <div style="margin-top: 5px; color: #666;">Conheça nossa loja</div>
+        </a>
+      </div>
+
+      <div class="wca-iconshome">
+        <a href="http://lojavirtual.digital/modelos" target="_blank">
+          <div><img src="/image/catalog/webca/home-nav-template.png"></div>
+          <div style="margin-top: 5px; color: #666;">Modelos</div>
+        </a>
+      </div>
+
+      <div class="wca-iconshome">
+        <a href="http://lojavirtual.digital#planos" target="_blank">
+          <div><img src="/image/catalog/webca/home-nav-plan.png"></div>
+          <div style="margin-top: 5px; color: #666;">Nossos planos</div>
+        </a>
+      </div>
+
+      <div class="wca-iconshome">
+        <a href="http://lojavirtual.digital/profissional" target="_blank">
+          <div><img src="/image/catalog/webca/home-nav-cart.png"></div>
+          <div style="margin-top: 5px; color: #666;">Comprar Loja Virtual</div>
+        </a>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="container" style="background-color: white;">   
+  	<div class="footer-contato wca-font">
+  		<a href="//lojavirtual.digital/profissional" target="_blank">
+        <img src="img/plano_profissional.png" style="width: 390px; margin-top: 70px;">
+      </a>
+  	</div>
+  	<div class="footer-contato wca-font">
+  		<a href="//lojavirtual.digital/service" target="_blank">
+        <img src="img/plano_service.png" style="width: 390px; margin-top: 70px;">
+      </a>
+  	</div>
+  </div>
+
+<footer>
+
+  <div class="container" style="background-color: white;">
+    <div class="footer-contato wca-font">
+      <img src="/image/catalog/webca/icon-tel.png" width="50">
+      &nbsp;Central de atendimento: 11 2376-0583
+    </div>
+    <div class="footer-contato wca-font" style="text-align: right;">
+      <img src="/image/catalog/webca/icon-mail.png" width="50">
+      &nbsp;E-mail: contato@lojavirtual.digital
+      
+    </div>
+  </div>
+
+
+  <div class="container" style="background-color: #f6ae3b; padding-top: 30px; padding-bottom: 15px; border-top-left-radius: 15px; border-top-right-radius: 15px; padding: 10px;">
+    
+    <div class="wca-footer-mid">
+      <p>LojaVirtual .digital - WebCA Internet &copy; 2011 - 2015</p> 
+    </div>
+    <div class="wca-footer-mid" style="text-align: right;">
+      <a href="//webca.com.br/" target="_blank"><div class="wca-logo-white">&nbsp;</div></a>
+    </div>
+
+    <hr style="border: 0px; border-top: solid 1px; color: #FFF; ">
+    <p style="text-align: justify;">* O plano WebLoja Services no valor de R$ 299,00 esta disponível apenas para contratação com prazo mínimo de 12 meses. O pagamento é feito mensalmente, e após o periodo de 12 meses a renovação do suporte é feita automaticamente mês a mês no mesmo valor mensal. Para cancelamento da assinatura deve ser solicitado através de nosso canal de atendimento ** A contratação de hora adicional de desenvolvimento deve ser orçada através de nosso canal de atendimento.</p>
+  </div>
+</footer>
+
+
+</body>
+
+</html>
