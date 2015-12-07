882bc2cc78d31279788715c4c9826861e8f25233
root <root@uspro575.startdedicated.com>
2015-12-07 02:05:21 -0200

    lv

--- public/catalog/language/portuguese-br/checkout/success.php
+++ public/catalog/language/portuguese-br/checkout/success.php
@@ -3,8 +3,42 @@
 $_['heading_title'] = 'O Pedido foi Cadastrado!';
 
 // Text
-$_['text_customer'] = '<p>Seu pedido foi processado com sucesso!</p><p>Caso tenha criado uma conta você pode ver o histórico dos seus pedidos acessando <a href="%s">sua conta</a> e clicando em <a href="%s">Histórico de Pedidos</a>.</p><p>Se seu pedido está associado a downloads, acesse os <a href="%s">Meus Downloads</a> para vê-los.</p><p>Por favor, entre em contato conosco caso tenha dúvidas clicando <a href="%s">aqui</a>.</p><p>Obrigado por comprar em nossa loja!</p>';
-$_['text_guest']    = '<p>Seu pedido foi processado com sucesso!</p><p>Caso você tenha dúvidas entre em contato conosco clicando <a href="%s">aqui</a>.</p><p>Obrigado por comprar em nossa loja!</p>';
+$_['text_customer'] = '<p>Seu pedido foi processado com sucesso!</p><p>Caso tenha criado uma conta você pode ver o histórico dos seus pedidos acessando <a href="%s">sua conta</a> e clicando em <a href="%s">Histórico de Pedidos</a>.</p><p>Se seu pedido está associado a downloads, acesse os <a href="%s">Meus Downloads</a> para vê-los.</p><p>Por favor, entre em contato conosco caso tenha dúvidas clicando <a href="%s">aqui</a>.</p><p>Obrigado por comprar em nossa loja!</p>'.'<!-- Google Code for Compra Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 975811980;
+var google_conversion_language = "en";
+var google_conversion_format = "3";
+var google_conversion_color = "ffffff";
+var google_conversion_label = "xUcUCNiZ6mAQjOum0QM";
+var google_remarketing_only = false;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/975811980/?label=xUcUCNiZ6mAQjOum0QM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>';
+$_['text_guest']    = '<p>Seu pedido foi processado com sucesso!</p><p>Caso você tenha dúvidas entre em contato conosco clicando <a href="%s">aqui</a>.</p><p>Obrigado por comprar em nossa loja!</p>'.'<!-- Google Code for Compra Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 975811980;
+var google_conversion_language = "en";
+var google_conversion_format = "3";
+var google_conversion_color = "ffffff";
+var google_conversion_label = "xUcUCNiZ6mAQjOum0QM";
+var google_remarketing_only = false;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/975811980/?label=xUcUCNiZ6mAQjOum0QM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>';
 $_['text_basket']   = 'Meu Carrinho';
 $_['text_checkout'] = 'Finalizar Compra';
 $_['text_success']  = 'Sucesso';
--- public/catalog/view/theme/default/template/common/footer.tpl
+++ public/catalog/view/theme/default/template/common/footer.tpl
@@ -29,4 +29,20 @@
   </div>
 </footer>
 
+<!-- Código do Google para tag de remarketing -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 975811980;
+var google_custom_params = window.google_tag_params;
+var google_remarketing_only = true;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/975811980/?value=0&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
 </body></html>
